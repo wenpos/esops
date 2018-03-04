b52c5a1ca3b7e1c1549a90f6ce069160a191c4af
fanfan <821078797@qq.com>
2018-03-04 10:15:07 +0800

    init project setup.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # esops
+distribution: python setup.py sdist
+--will generate zip on windows or tar.gz on unix
+reference: https://docs.python.org/2/distutils/introduction.html#distutils-simple-example
+           https://docs.python.org/2/distutils/setupscript.html#setup-script
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='esops',
+      version='1.0',
+      description='a tool for elasticsearch ops',
+      author='wenpos',
+      author_email='gward@python.net',
+      url='https://github.com/wenpos/esops',
+      packages=['esops'],
+     )
