3721da95fd815eb15ed234103f60e22cd6baf104
Fanfan <821078797@qq.com>
2018-03-25 16:26:22 +0800

    build project

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ distribution: python setup.py sdist
 reference: https://docs.python.org/2/distutils/introduction.html#distutils-simple-example
            https://docs.python.org/2/distutils/setupscript.html#setup-script
            https://docs.python.org/2/distutils/setupscript.html#meta-data
+
+           https://blog.lilydjwg.me/2013/3/18/package-python-scripts-using-cx_freeze.38221.html
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+import sys
 from setuptools import setup
-import py2exe
+
 
 def get_install_requires():
     res = ['elasticsearch>=5.4.0,<7.0.0']
@@ -8,23 +9,72 @@ def get_install_requires():
     return res
 
 
-setup(
-    name="esops",
-    version="1.0",
-    keywords=("elasticsearch", "ops"),
-    description="a tool for elasticsearch ops",
-    long_description="a tool for elasticsearch ops",
-    license="Apache License, Version 2.0",
+try:
+    from cx_Freeze import setup, Executable
 
-    author="wenpos",
-    author_email="test@test.com",
-    url="https://github.com/wenpos/esops",
+    try:
+        import certifi
 
-    packages=["esops"],
-    include_package_data=True,
-    install_requires=get_install_requires(),
-    entry_points={"console_scripts": [
-        "espos = esops.mian:main",
-    ]},
-    
-)
+        cert_file = certifi.where()
+    except ImportError:
+        cert_file = ''
+
+    base = None
+    if sys.platform == "win32":
+        base = "Win32GUI"
+
+    esops_exe =  Executable(
+        "esops/main.py",
+        base=base,
+        targetName = "esops",
+    )
+
+    buildOptions = dict(
+        packages=[],
+        excludes=[],
+        include_files=[cert_file],
+
+    )
+
+    setup(
+        name="esops",
+        version="1.0",
+        keywords=("elasticsearch", "ops"),
+        description="a tool for elasticsearch ops",
+        long_description="a tool for elasticsearch ops",
+        license="Apache License, Version 2.0",
+
+        author="wenpos",
+        author_email="test@test.com",
+        url="https://github.com/wenpos/esops",
+
+        packages=["esops"],
+        include_package_data=True,
+        install_requires=get_install_requires(),
+        entry_points={"console_scripts": [
+            "espos = esops.mian:main",
+        ]},
+        options={"build_exe": buildOptions},
+        executables=[esops_exe],
+    )
+
+except ImportError:
+    setup(
+        name="esops",
+        version="1.0",
+        keywords=("elasticsearch", "ops"),
+        description="a tool for elasticsearch ops",
+        long_description="a tool for elasticsearch ops",
+        license="Apache License, Version 2.0",
+
+        author="wenpos",
+        author_email="test@test.com",
+        url="https://github.com/wenpos/esops",
+
+        packages=["esops"],
+        include_package_data=True,
+        install_requires=get_install_requires(),
+        entry_points={"console_scripts": [
+            "espos = esops.mian:main",
+        ]}
+    )
